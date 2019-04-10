444f1abf95f9050d19a76c5962ccf295443c428f
Surya Teja Kavutarapu <suryateja.k@scriptbees.com>
2019-04-10 14:40:25 +0530

    Intial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Angulartics2Module } from 'angulartics2';
+import { environment } from '@env/environment';
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { LoginModule } from './login/login.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('./ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    NgbModule.forRoot(),
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    LoginModule,
+    Angulartics2Module.forRoot([]),
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
